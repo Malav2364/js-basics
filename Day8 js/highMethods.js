a2c94a3b15c2a7ecb6b87240dbe3c1e5210121cd
Malav Patel <95128635+Malav2364@users.noreply.github.com>
2025-01-31 16:32:20 +0530

    completed tasks

--- Day8 js/highMethods.js
+++ Day8 js/highMethods.js
@@ -1,8 +1,12 @@
 //using map reduce and filter on an array 
-const nums = [1,2,3,4,5,6,7,8,9,10]
+const nums = [1,2,3,4,5]
 let sqnum = nums.map(num => num*num)
 let even =  nums.filter(num => num % 2 == 0)
 let sum = nums.reduce((acc,curr)=>acc+curr,0)
+let gtfive = nums.filter(num=> num > 3)
+let product = nums.reduce((acc, curr)=> acc*curr, 1)
+console.log(product)
+console.log(gtfive)
 console.log(sum)
 console.log(even)
 console.log(sqnum)
